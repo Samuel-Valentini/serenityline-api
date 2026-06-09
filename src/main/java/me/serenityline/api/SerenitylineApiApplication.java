518a9e92db3ab8b926dade8083cec60af17e9ed0
Samuel-Valentini <samuelvitalia@gmail.com>
2026-06-09 16:38:52 +0200

    chore: disable default Spring Security user

--- src/main/java/me/serenityline/api/SerenitylineApiApplication.java
+++ src/main/java/me/serenityline/api/SerenitylineApiApplication.java
@@ -5,9 +5,10 @@ package me.serenityline.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class SerenitylineApiApplication {
 
     public static void main(String[] args) {
